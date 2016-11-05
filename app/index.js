63a911c19f5fd902a09c43b3430b0f29320082bc
Callum Flack <callumflack@gmail.com>
2016-11-05 12:50:05 +1000

    lesson run-thru

--- app/index.js
+++ app/index.js
@@ -1,58 +1,23 @@
-var USER_DATA = {
-  name: 'Millinius Smith',
-  username: 'johnsmith',
-  image: 'http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTIwNjA4NjMzOTc0MTk1NzI0.jpg'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+var Raven = require('raven-js');
 
-// fn(data)=view
+var sentryKey = 'b90234238cb744d6b8cdf782c6e2ba3e'
+var sentryApp = '112159'
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
 
-// Your components shoudl be:
-// Focused
-// Independent
-// Reusable
-// Small
-// Testable
+var _APP_INFO = {
+    name: 'Github Battle',
+    brand: 'video4',
+    version: '1.0'
+}
 
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
+Raven.config(sentryURL, {
+    release: _APP_INFO.version,
+    tags: {
+        branch: _APP_INFO.branch
+    }
+}).install()
 
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function () {
-    return <div>{this.props.name}</div>
-  }
-});
-
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
-
-ReactDOM.render(
-  <Avatar user={USER_DATA} />,
-  document.getElementById('app')
-);
+ReactDOM.render( routes, document.getElementById('app'));
